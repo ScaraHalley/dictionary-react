43b84a9fd05df40a100bc2185bb092ad28844357
ScaraHalley <106163549+ScaraHalley@users.noreply.github.com>
2022-08-08 14:04:03 +1000

    basic search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header">hello from app</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">coded by Sarah Halley</footer>
+      </div>
     </div>
   );
 }
