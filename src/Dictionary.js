d201e33665795d1501c9474a7ae5c0af110e3c5d
ScaraHalley <106163549+ScaraHalley@users.noreply.github.com>
2022-08-11 22:34:28 +1000

    photo search and styling

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,33 +1,47 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "./Results";
+import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelApiKey = `563492ad6f91700001000001659d5699509743dc93d985faa5081c8e`;
+    let pexelApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+    let headers = { Authorization: `Bearer ${pexelApiKey}` };
+    axios.get(pexelApiUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     search();
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
+
   function load() {
     setLoaded(true);
     search();
   }
+
   if (loaded) {
     return (
       <div className="Dictionary">
@@ -44,6 +58,7 @@ export default function Dictionary(props) {
           <div className="hint">suggested words: sunset, wine, moon...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
